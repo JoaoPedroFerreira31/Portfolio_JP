ce4d3283a12b24946b9c8a9f50b7f8629236e1a8
Joao Pedro Ferreira <joaopedroferreira@iMac-de-Joao.lan>
2020-09-15 15:14:33 +0100

    init homepage

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");  
+
+// ==============
+//     ROUTES
+// ==============
+
+//Root Route
+app.get("/", function(req, res){
+    res.redirect("home");
+});
+
+//Home 
+app.get("/home", function(req, res){ 
+    res.render("home");
+});
+
 //Server Started 
 app.listen(process.env.PORT, process.env.IP, function(){ 
     console.log("Server has started");
 });
+console.log(process.env.PORT);
